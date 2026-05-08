106da984fc94ddc9e1d106ef1a7142d2b30d6014
shahid munir mangole <shahidsofficial07@gmail.com>
2026-05-08 23:51:50 +0530

    add time & space complexity for ref

--- Fast & Slow Pointers/Linked List/HappyNumber.java
+++ Fast & Slow Pointers/Linked List/HappyNumber.java
@@ -1,10 +1,11 @@
-public class HappyNumber{
+public class HappyNumber {
 
+    // Time Complexity: O(log n)
+    // Space Complexity: O(1)
 
+    public static boolean isHappy(int n) {
 
-    public static boolean isHappy(int n){
-
-        int fast  = n;
+        int fast = n;
         int slow = n;
 
         while (fast != 1) {
@@ -13,30 +14,26 @@ public class HappyNumber{
             fast = next(fast);
             fast = next(fast);
 
-            if(slow == fast && fast != 1){
-                return  false;
+            if (slow == fast && fast != 1) {
+                return false;
             }
         }
-
+        
         return true;
     }
 
-
-    public static int  next(int n){
+    public static int next(int n) {
         int sum = 0;
-        
         while (n > 0) {
-            int digit  = n % 10;
-            n = n/10;
-            sum  += digit * digit;
+            int digit = n % 10;
+            n = n / 10;
+            sum += digit * digit;
         }
         return sum;
     }
 
-
-
     public static void main(String[] args) {
-        int num = 19; //2
+        int num = 19; // 2
         System.out.println(isHappy(num));
     }
 }
