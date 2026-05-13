eba822c43328dfd4a4afd2fddae831f05aa14170
shahid munir mangole <shahidsofficial07@gmail.com>
2026-05-13 17:27:48 +0530

    Two Sum

---
+++ Two Pointers/TwoSum.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+class TwoSum {
+
+    public static int[] twoSum(int arr[], int target) {
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+
+            if (sum == target) {
+                return new int[] { arr[left], arr[right] };
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return new int[] {-1 , -1};
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 5, 7, 8, 9 };
+        int target = 15;
+        System.out.println(Arrays.toString(twoSum(arr, target)));
+    }
+}
