5539ba645a235acc84a6b94b86511042ec0001de
shahid munir mangole <shahidsofficial07@gmail.com>
2026-05-15 11:17:40 +0530

    Remove duplicates from sorted LL

---
+++ Two Pointers/RemoveDuplicateNodes.java
@@ -0,0 +1,35 @@
+public class RemoveDuplicateNodes {
+
+    public static ListNode deleteDuplicatesNode1(ListNode head) {
+
+        ListNode current = head;
+
+        while (current != null && current.next != null) {
+
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(3);
+
+        head = deleteDuplicatesNode1(head);
+
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+
+    }
+}
